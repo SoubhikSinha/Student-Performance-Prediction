6c169d2ea32eec28e463642b38f7434bab56f688
sricks404 <hackos.sinha@gmail.com>
2024-08-28 21:12:38 -0400

    Docker + Azure files added

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def predict_datapoint():
 
 # Lets run app.py
 if __name__=="__main__":
-    # app.run(host="0.0.0.0", debug = True)
+    # app.run(host="0.0.0.0", debug = True, port = 5000)
     app.run(host="0.0.0.0") # Debug mode should be made inactive during deployment   
